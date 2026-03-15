c49d8bf2e33abfb27ed456619bebdf1ffe2e7029
BadL19 <109616436+BadL19@users.noreply.github.com>
2026-03-15 14:18:54 -0400

    added proper url

--- backend/main.py
+++ backend/main.py
@@ -10,10 +10,11 @@ app = FastAPI(title="Caribou Log Cabin Resort API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173", 
+        "http://localhost:5173",
         "http://localhost:3000",
-        "https://caribou-resort1.onrender.com/"            
-     ],
+        "https://caribou-resort1.onrender.com",
+        "https://caribou-resort.onrender.com",
+    ],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
