2f87fbf07cbca8e8140659437f002543c74484c7
BadL19 <109616436+BadL19@users.noreply.github.com>
2026-03-15 13:32:27 -0400

    fix cors

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,11 @@ app = FastAPI(title="Caribou Log Cabin Resort API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:5173", 
+        "http://localhost:3000",
+        "https://caribou-resort1.onrender.com/"            
+     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
