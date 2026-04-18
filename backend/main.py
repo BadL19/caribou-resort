42a727332759a2b76b3c8f502d160a8050b9bf90
BadL19 <109616436+BadL19@users.noreply.github.com>
2026-04-18 14:09:27 -0400

    update cors for vercel

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,7 @@ app.add_middleware(
         allow_origins=[
         "http://localhost:5173",
         "http://localhost:3000",
-        "caribou-git-main-badl19s-projects.vercel.app"
+        "https://caribou-sooty.vercel.app"
         "https://caribou-resort1.onrender.com",
     ],
     allow_credentials=True,
